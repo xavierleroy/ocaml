af068321181ce1789e8e02ffc0ab898366d447e2
Xavier Leroy <xavier.leroy@college-de-france.fr>
2023-06-18 10:00:46 +0200

    Use `of_hex` instead of `from_hex` in the new API
    `Digest.from_hex` remains available as an alias for `Digest.of_hex`.

--- stdlib/digest.ml
+++ stdlib/digest.ml
@@ -34,7 +34,7 @@ let string_of_hex s =
     | '0'..'9' -> Char.code c - Char.code '0'
     | 'A'..'F' -> Char.code c - Char.code 'A' + 10
     | 'a'..'f' -> Char.code c - Char.code 'a' + 10
-    | _ -> invalid_arg "Digest.from_hex" in
+    | _ -> invalid_arg "Digest.of_hex" in
   let byte i = digit s.[i] lsl 4 + digit s.[i+1] in
   String.init (String.length s / 2) (fun i -> Char.chr (byte (2 * i)))
 
@@ -54,7 +54,7 @@ module type S = sig
   val output : out_channel -> t -> unit
   val input : in_channel -> t
   val to_hex : t -> string
-  val from_hex : string -> t
+  val of_hex : string -> t
   type state
   val create : unit -> state
   val get_digest : state -> t
@@ -154,8 +154,8 @@ let to_hex d =
   if String.length d <> hash_size then invalid_arg "Digest.to_hex";
   hex_of_string d
 
-let from_hex s =
-  if String.length s <> hash_size * 2 then invalid_arg "Digest.from_hex";
+let of_hex s =
+  if String.length s <> hash_size * 2 then invalid_arg "Digest.of_hex";
   string_of_hex s
 
 end
@@ -206,3 +206,5 @@ module MD5 = Make(MD5_hash)
 (* Default exported implementation is MD5 *)
 
 include MD5
+
+let from_hex = of_hex
